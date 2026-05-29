0657684a148e52237e1788d6a206208729400f05
vatsarun-dev <vatsarun58@gmail.com>
2026-05-29 21:46:30 +0530

    learning CRUD operation

--- CRUD/server.js
+++ CRUD/server.js
@@ -1,2 +1,7 @@
 // /export the app
 let app = require("./src/app.js");
+
+// starting the server
+app.listen(3000, () => {
+  console.log("app is running ");
+});
--- CRUD/src/app.js
+++ CRUD/src/app.js
@@ -9,6 +9,45 @@ let app = express();
 
 app.use(express.json());
 
+// creating an array to perform crud operation
+let students = [];
+
+// creating apis
+
+// post api
+app.post("/students", (req, res) => {
+  students.push(req.body);
+  return res.status(201).json({
+    message: "students created successfully",
+  });
+});
+
+// get api
+app.get("/students/get", (req, res) => {
+  return res.status(200).json({
+    students,
+  });
+});
+
+// patch api
+app.patch("/students/update/:index", (req, res) => {
+  let { index } = req.params;
+  let { phone } = req.body;
+  students[0][index].phone = phone;
+  return res.status(201).json({
+    message: "students update successfully",
+  });
+});
+
+// delete api
+app.delete("/students/delete/:index", (req, res) => {
+  let { index } = req.params;
+  students.splice(index, 1);
+  return res.status(201).json({
+    message: "students delete successfully",
+  });
+});
+
 // we have to export the app to use in server.js
 
 module.exports = app;
