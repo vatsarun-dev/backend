e6548fb33ac7dba473eba4dd331a1beeb8843ee1
vatsarun-dev <vatsarun58@gmail.com>
2026-06-11 21:53:53 +0530

    initializing jwt and cookies

--- authentication/server/src/app.js
+++ authentication/server/src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = require("./routes/user.routes.js");
+const cookieParser = require("cookie-parser");
 const app = express();
 app.use(express.json());
-
+app.use(cookieParser());
 app.use("/api/user", router);
 module.exports = app;
--- authentication/server/src/controllers/user.controller.js
+++ authentication/server/src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const userModel = require("../models/user.models.js");
+const jwt = require("jsonwebtoken");
 const createUserController = async (req, res) => {
   try {
     let { name, mobile, email, password } = req.body;
@@ -8,7 +9,7 @@ const createUserController = async (req, res) => {
         message: "All fields are required",
       });
 
-    const isExisted = await userModel.findOne({
+    let isExisted = await userModel.findOne({
       email,
     });
     if (isExisted)
@@ -16,22 +17,26 @@ const createUserController = async (req, res) => {
         message: "The user is already existed",
       });
 
-    const newUser = await userModel.create(
-      {
-        name,
-        mobile,
-        email,
-        password,
-      },
-      {
-        new: true,
-      },
-    );
+    const newUser = await userModel.create({
+      name,
+      mobile,
+      email,
+      password,
+    });
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_TOKEN, {
+      expiresIn: "1h",
+    });
+    console.log(token);
+
+    res.cookie("token", token);
+
     return res.status(201).json({
       message: "User created successfully",
       newUser,
     });
   } catch (error) {
+    console.log(error);
     return res.status(500).json({
       message: "Internal Server Error",
     });
