b7210d33ece2ddaa5f0579e0998ed84402bdbd5b
vatsarun-dev <vatsarun58@gmail.com>
2026-07-01 17:36:59 +0530

    implementing own error class using inherit the Error class

--- GEH/src/app.js
+++ GEH/src/app.js
@@ -6,10 +6,13 @@ app.use(express.json());
 
 app.use("/api/user", router);
 
+// it should be placed after the api not the first one
 // global error handler
 app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: err.message,
+  let statusCode = err.statusCode || 500;
+  let message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    message: message,
   });
 });
 module.exports = app;
--- GEH/src/controllers/user.controller.js
+++ GEH/src/controllers/user.controller.js
@@ -1,10 +1,11 @@
 const userModel = require("../models/user.model");
 const asyncHandler = require("../utils/asyncHandler");
+const ApiError = require("../utils/errorHandler");
 const registerController = asyncHandler(async (req, res) => {
   let { name, email, password } = req.body;
 
   if (!name || !email || !password)
-    return res.status(401).json({ message: "All fields are required" });
+    throw new ApiError(404, "All fields are required");
 
   let isExisted = await userModel.findOne({ email });
 
