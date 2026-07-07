7beb51fbeef3ca9ec522ada4210c2e0e6d4f2620
vatsarun-dev <vatsarun58@gmail.com>
2026-07-07 19:06:23 +0530

    implementing the logic for multiple files

--- FTP_INTEGERATION/client/src/App.jsx
+++ FTP_INTEGERATION/client/src/App.jsx
@@ -2,19 +2,41 @@ import React from "react";
 import axiosInstance from "./config/axiosInstance";
 import { useState } from "react";
 const App = () => {
-  const [data, setData] = useState(null);
-  const formData = new FormData();
-  formData.append("image", data);
+  // THIS PART IS FOR SINGLE FILE ACCEPTANCE
+
+  // const [data, setData] = useState(null);
+  // const formData = new FormData();
+  // formData.append("image", data);
+  // const handleSubmit = async () => {
+  //   try {
+  //     console.log("button clicked");
+  //     const res = await axiosInstance.post("/file/upload-files", formData);
+  //     console.log(res);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  // THIS IS FOR MULTIPLE FILE TO SEND
+
+  const [data, setData] = useState([]);
+
   const handleSubmit = async () => {
     try {
-      console.log("button clicked");
-      const res = await axiosInstance.post("/file/upload-files", formData);
+      const formData = new FormData();
+      for (let file of data) {
+        formData.append("image", file);
+      }
+
+      const res = await axiosInstance.post(
+        "/file/multiple-upload-files",
+        formData,
+      );
       console.log(res);
     } catch (error) {
       console.log(error);
     }
   };
-
   return (
     <div className="bg-black min-h-screen gap-3 text-white text-4xl font-bold flex flex-col justify-center items-center">
       Send file{" "}
@@ -22,7 +44,10 @@ const App = () => {
         type="file"
         name=""
         id=""
-        onChange={(e) => setData(e.target.files[0])}
+        multiple
+        // THIS IS ALSO FOR SINGLE FILE ACCEPTANCE
+        // onChange={(e) => setData(e.target.files[0])}
+        onChange={(e) => setData(e.target.files)}
       />
       <button onClick={handleSubmit} className="text-white bg-gray-800 p-4">
         Submit
