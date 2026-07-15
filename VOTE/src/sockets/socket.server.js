e19f9e12e6fa439f2567f40667b78f2ccd195c3f
vatsarun-dev <vatsarun58@gmail.com>
2026-07-15 16:15:14 +0530

    adding the functionality that only one user can vote one time

--- VOTE/src/sockets/socket.server.js
+++ VOTE/src/sockets/socket.server.js
@@ -6,13 +6,19 @@ const voteCount = {
   no: 0,
 };
 
+const vote = [];
+
 export default function initSocket(httpServer) {
   const io = new Server(httpServer);
   io.on("connection", (socket) => {
     console.log("socket is connected successfully");
     // THIS CUSTOM ACTION IS USED TO COUNT THE YES
     socket.on("yes-btn", () => {
+      // THIS CHECK IS TO BE ENSURE THAT ONE USER CAN ONLY VOTE ONLY ONE TIME
+      if (vote.includes(socket.id)) return;
       voteCount.yes += 1;
+
+      vote.push(socket.id);
       // THIS EMIT ACTION FIRE IS USED TO DISPLAY THE ALL VOTE COUNT
       io.emit("vote-display", voteCount);
     });
@@ -20,7 +26,9 @@ export default function initSocket(httpServer) {
     // THIS CUSTOM ACTION IS USED TO COUNT THE YES
 
     socket.on("no-btn", () => {
+      if (vote.includes(socket.id)) return;
       voteCount.no += 1;
+      vote.push(socket.id);
 
       io.emit("vote-display", voteCount);
     });
