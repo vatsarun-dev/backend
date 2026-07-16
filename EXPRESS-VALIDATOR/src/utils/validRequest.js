cde10af9aee89fe28c6c195d8f27c101b3391e01
vatsarun-dev <vatsarun58@gmail.com>
2026-07-16 20:55:40 +0530

    update validRequest logic

--- EXPRESS-VALIDATOR/src/utils/validRequest.js
+++ EXPRESS-VALIDATOR/src/utils/validRequest.js
@@ -1,1 +1,8 @@
+import { validationResult } from "express-validator";
 
+export const validRequest = (req, res, next) => {
+  const error = validationResult(req);
+
+  if (!error.isEmpty()) return res.status(422).json({ error: error.array() });
+  next();
+};
