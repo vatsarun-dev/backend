7cd3230f9063b9a63692729800d8acccfaef89ec
vatsarun-dev <vatsarun58@gmail.com>
2026-07-22 19:40:01 +0530

    implementing docker compose

--- DOCKER/src/routes/app.route.js
+++ DOCKER/src/routes/app.route.js
@@ -5,4 +5,13 @@ appRoutes.get("/", (req, res) => {
   res.json("hii from docker");
 });
 
+appRoutes.get("/test", (req, res) => {
+  res.json("testing successfully");
+});
+appRoutes.get("/testing-1", (req, res) => {
+  res.json("testing successfully");
+});
+appRoutes.get("/testing-2", (req, res) => {
+  res.json("testing successfully");
+});
 export default appRoutes;
