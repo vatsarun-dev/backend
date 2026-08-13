3b3de021a73ba42edc8b0480343e57ab23a5ce53
vatsarun-dev <vatsarun58@gmail.com>
2026-08-13 23:20:50 +0530

    feat(first programe of typescript)

---
+++ typescript/index.js
@@ -0,0 +1,6 @@
+// const a: string = "this is my first  typescript server";
+// console.log(a);
+const a = "hello";
+console.log(typeof a);
+export {};
+//# sourceMappingURL=index.js.map
---
+++ typescript/index.ts
@@ -0,0 +1,4 @@
+// const a: string = "this is my first  typescript server";
+// console.log(a);
+const a: string = "hello";
+console.log(typeof a);
