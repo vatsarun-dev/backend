d6f00adf700e86d3491c2154d67e2f41ce292d8b
vatsarun-dev <vatsarun58@gmail.com>
2026-08-18 21:34:08 +0530

    feat(function parameter and return typing)

--- typescript/index.js
+++ typescript/index.js
@@ -1,14 +1,21 @@
-// const a: string = "this is my first  typescript server";
-// console.log(a);
-const a = "hello";
-console.log(typeof a);
-const arun = {
-    name: "arun",
-    phone: 12345,
-};
-const a;
-if (a === number)
-    console.log("a is number");
-console.log("a is string");
+// // const a: string = "this is my first  typescript server";
+// // console.log(a);
+// const a: string = "hello";
+// console.log(typeof a);
+// interface object {
+//   name: string;
+//   phone: number;
+// }
+// const arun: object = {
+//   name: "arun",
+//   phone: 12345,
+// };
+// const b: number | string;
+// if (b === "number") console.log("a is number");
+// console.log("a is string");
+function total(price, qty) {
+    return price * qty;
+}
+total(10, 20);
 export {};
 //# sourceMappingURL=index.js.map
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,17 +1,22 @@
-// const a: string = "this is my first  typescript server";
-// console.log(a);
-const a: string = "hello";
-console.log(typeof a);
+// // const a: string = "this is my first  typescript server";
+// // console.log(a);
+// const a: string = "hello";
+// console.log(typeof a);
 
-interface object {
-  name: string;
-  phone: number;
+// interface object {
+//   name: string;
+//   phone: number;
+// }
+// const arun: object = {
+//   name: "arun",
+//   phone: 12345,
+// };
+
+// const b: number | string;
+// if (b === "number") console.log("a is number");
+// console.log("a is string");
+
+function total(price: number, qty: number): number {
+  return price * qty;
 }
-const arun: object = {
-  name: "arun",
-  phone: 12345,
-};
-
-const b: number | string;
-if (b === "number") console.log("a is number");
-console.log("a is string");
+total(10, 20);
