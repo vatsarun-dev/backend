92170206968a756029722132132f8fd45648defa
vatsarun-dev <vatsarun58@gmail.com>
2026-08-21 21:29:22 +0530

    feat(adding functionality of asking manually question)

--- genai/index.ts
+++ genai/index.ts
@@ -1,15 +1,23 @@
 import env from "./env.ts";
+import rl from "readline/promises";
 import { ChatMistralAI } from "@langchain/mistralai";
 
 if (!env.Mistral_api_key) throw new Error("there is no api key");
+
+const readline = rl.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 const model = new ChatMistralAI({
   apiKey: env.Mistral_api_key,
   model: "mistral-medium-latest",
 });
 
-const message: string = await model.stream(
-  "write a letter to pm for congrats that you are most successfully minister",
-);
+while (true) {
+  const message: string = await readline.question("Enter your query: ");
+  const responses: string = await model.stream(message);
+  for await (const chunk of responses) process.stdout.write(chunk.text);
 
-for await (const chunk of message) process.stdout.write(chunk.text);
+  process.stdout.write("\n");
+}
