675f2d25ed01ed86b86b76327d5084c8af3d27f1
vatsarun-dev <vatsarun58@gmail.com>
2026-08-25 17:06:29 +0530

    feat(initializing first tool with LLM)

--- agentic-ai/index.ts
+++ agentic-ai/index.ts
@@ -1,7 +1,11 @@
 import { ChatMistralAI } from "@langchain/mistralai";
-import { HumanMessage, AIMessage, createAgent } from "langchain";
+import { HumanMessage, AIMessage, createAgent, tool } from "langchain";
 import env from "./env.ts";
 import rl from "readline/promises";
+import z from "zod";
+/**
+ * @vatsarun-dev
+ */
 const model = new ChatMistralAI({
   apiKey: env.Mistral_api_key,
   model: "mistral-medium-latest",
@@ -12,9 +16,22 @@ const readline = rl.createInterface({
   output: process.stdout,
 });
 
-const agent = createAgent({ model });
 const chatHistory: (HumanMessage | AIMessage)[] = [];
 let response = "";
+
+async function getWeather(city: string): Promise<string> {
+  return JSON.stringify({ city: city, temperature: "25", condition: "sunny" });
+}
+
+const weatherTool = tool(getWeather, {
+  name: "getWeather",
+  description: "to give the weather of the city",
+  schema: z.object({
+    city: z.string().describe("get the city name for weather "),
+  }),
+});
+
+const agent = createAgent({ model, tools: [weatherTool] });
 
 while (true) {
   const question: string = await readline.question("Enter your query: ");
